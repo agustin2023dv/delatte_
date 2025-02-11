c1660ca15dbbc0133ebc4b1776f8cb9e649c507e
lualzugaray <lucia.alzugaray24@gmail.com>
2025-02-11 01:13:54 -0300

    refactor(navigation): improve ProfileTabs structure and styles

--- app/(profile)/customer-profile/ProfileTabs.tsx
+++ app/(profile)/customer-profile/ProfileTabs.tsx
@@ -1,28 +1,52 @@
-  import React from 'react';
-  import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-  import ReservationsTab from './ReservationsTab';
-  import AccountSettings from './AccountSettingsTab';
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { StyleSheet } from "react-native";
+import ReservationsTab from "./ReservationsTab";
+import AccountSettings from "./AccountSettingsTab";
 
-  const Tab = createMaterialTopTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-  export default function ProfileTabs() {
-    return (
-      <Tab.Navigator>
+export default function ProfileTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: styles.tabBar, 
+        tabBarIndicatorStyle: styles.indicator, 
+        tabBarLabelStyle: styles.label, 
+        tabBarActiveTintColor: "#6B4226", 
+        tabBarInactiveTintColor: "#A58D7F", 
+      }}
+    >
+      <Tab.Screen
+        name="AccountSettings"
+        component={AccountSettings}
+        options={{ title: "Información de la cuenta" }}
+      />
+      <Tab.Screen
+        name="ReservationsTab"
+        component={ReservationsTab}
+        options={{ title: "Mis Reservas" }}
+      />
+    </Tab.Navigator>
+  );
+}
 
-<Tab.Screen 
-  name="AccountSettings" 
-  component={AccountSettings} 
-  options={{ title: "Información de la cuenta" }} 
-/>
-
-        <Tab.Screen 
-  name="ReservationsTab" 
-  component={ReservationsTab} 
-  options={{ title: "Mis Reservas" }} 
-/>
-
-
-        
-      </Tab.Navigator>
-    );
-  }
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: "#F7EBE1", 
+    elevation: 3, 
+    shadowOpacity: 0.1,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E0D6D1",
+  },
+  indicator: {
+    backgroundColor: "#6B4226", 
+    height: 3, 
+    borderRadius: 2,
+  },
+  label: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 14,
+    textTransform: "none",
+  },
+});
